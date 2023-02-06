833607593ec1beb39fea8f83b9d45e1bb3c0245e
Chukwuebuka Osuji <74817012+Bukasuji@users.noreply.github.com>
2023-02-06 17:16:40 +0100

    initial commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x55c95bD6596EA1DEb2244b1E52E1C37F48552C23";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
